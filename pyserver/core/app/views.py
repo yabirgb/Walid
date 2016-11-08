65c145c277f1bd8e36e9a2ef2b83e67a1d815208
yabirgb <zanklord@gmail.com>
2016-11-08 13:23:19 +0100

    Added filters

--- pyserver/core/app/views.py
+++ pyserver/core/app/views.py
@@ -1,10 +1,15 @@
-from flask import render_template
+from flask import render_template, redirect, request
 from pymongo import MongoClient
 from app import app
 
 client = MongoClient('localhost', 27017)
 db = client.test
 collection = db.users
+
+
+@app.template_filter('urls_completer')
+def urls_completer(url):
+    return url if "://" in url else "//" + url
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +21,13 @@ def user_links(secret):
     user = collection.find_one({"secret": secret})
     return render_template('links.html',
         user=user)
+
+@app.route('/search/', methods=['GET', 'POST'])
+def user_search():
+    if request.args.get("code"):
+        return redirect("/secret/{}".format(request.args.get("code")))
+    return render_template('search.html')
+ 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
