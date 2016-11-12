1e3b41f1e65a9486de1b5717504e7862fdf074a0
yabirgb <zanklord@gmail.com>
2016-11-12 23:29:32 +0100

    Changed port

--- pyserver/core/app/__init__.py
+++ pyserver/core/app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 app.static_folder = 'static'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.debug = True
 
 from app import views
+
+port = int(os.environ.get('PORT', 8000))
+app.run(host='0.0.0.0', port=port)
