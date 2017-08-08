dc1ef72dbe4418dabf0ba2e21af81aed95630dd1
yabir <yabirgb@gmail.com>
2017-08-08 22:56:30 +0200

    Added commands to manage database

--- telegram_bot/manage_database.py
+++ telegram_bot/manage_database.py
@@ -1,10 +1,20 @@
 from peewee import *
 from models import *
 
-
 def start():
-    if not User.table_exists():
-        db.create_tables([User, Link])
+    print("Starting creation of tables in " + str(DATABASE))
+    db.create_tables([User, Link, Map, Message], safe=True)
+    print("Tables created")
 
+def users_count():
+    print(len(User.select()))
 
-start()
+def clean_auth():
+    query = User.update(authCode=0).where()
+
+if __name__ == '__main__':
+    import sys
+    if sys.argv[1] == "start":
+        start()
+    elif sys.argv[1] == "users":
+        users_count()
