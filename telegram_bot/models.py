5f019f0ebce4eea7f038ac86906c1eb5a8d83c93
yabir <yabirgb@gmail.com>
2017-08-08 22:56:57 +0200

    Added Map and Comment models and also config to use in prod

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -3,8 +3,21 @@ import os
 from peewee import *
 
 DATABASE = os.environ.get("DATABASE", None)
-print(DATABASE)
-db = SqliteDatabase(DATABASE)
+
+if DATABASE != None:
+    db = SqliteDatabase(DATABASE)
+else:
+    DB_NAME = os.environ.get("DB", None)
+    DB_USER = os.environ.get("DBUSER", None)
+    DB_PASS = os.environ.get("DBPASS", None)
+    DB_HOST = os.environ.get("DBHOST", None)
+    db = PostgresqlDatabase(
+        DB_NAME,  # Required by Peewee.
+        user=DB_USER,  # Will be passed directly to psycopg2.
+        password=DB_PASS,  # Ditto.
+        host=DB_HOST,  # Ditto.
+    )
+
 db.connect()
 
 
@@ -12,6 +25,7 @@ class User(Model):
     username = CharField()
     telegramId = CharField(unique=True)
     secret = CharField(unique=True)
+    authCode = IntegerField()
     waitingReply = BooleanField(default=False)
     pocket_Token = CharField(null=True)
     pocket_configured = BooleanField(default=False)
@@ -26,5 +40,22 @@ class Link(Model):
     user = ForeignKeyField(User)
     reviewed = BooleanField(default=False)
 
+    class Meta:
+        database = db
+
+class Map(Model):
+    latitude = DoubleField()
+    longitude = DoubleField()
+    reviewed = BooleanField(default=False)
+    date = DateTimeField()
+    user = ForeignKeyField(User)
+    class Meta:
+        database = db
+
+class Message(Model):
+    date = DateTimeField()
+    text = CharField(max_length=500)
+    reviewed = BooleanField(default=False)
+    user = ForeignKeyField(User)
     class Meta:
         database = db
