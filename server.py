ba18364bc82707af45d56c8042a8d24a34754454
Yabir <yabirgb@gmail.com>
2017-08-09 20:35:55 +0000

    Configured to be used in prod

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 import os
 
 from pyserver.app.views import app
+from werkzeug.contrib.fixers import ProxyFix
 
 DEBUG = bool(os.environ.get('DEBUG', True))
 
-app.static_folder = 'static'
+app.static_folder = '/home/zanklord/webapps/walid/LinksBot/pyserver/static'
 app.config['TEMPLATES_AUTO_RELOAD'] = False
 app.debug = DEBUG
 
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
 if __name__ == '__main__':
     app.debug = DEBUG
-    host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 8000))
-    app.run(host=host, port=port)
+    app.run(host='localhost', port=port)
